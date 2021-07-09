8b4d4e9721264faecaf19314c1b9e7e4ef78dd83
MadhavDhinesh <87005021+MadhavDhinesh@users.noreply.github.com>
2021-07-09 17:57:58 +0530

    first commit

---
+++ PPMFsd/ppmtooll/src/main/java/io/MadhavDhinesh/ppmtooll/PpmtoollApplication.java
@@ -0,0 +1,13 @@
+package io.MadhavDhinesh.ppmtooll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoollApplication.class, args);
+	}
+
+}
